586f9a1a1df84310b1ed420af4184e575789f171
Jack Rong <rongyadong19900724@gmail.com>
2019-04-05 19:01:52 +1100

    add mock data

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,46 +1,16 @@
 import React, { Component } from 'react'
 import Project from './Project'
+import { data } from '../mock/data'
 
 class Projects extends Component {
   state = {
-    projects: [
-      [
-        'https://www.compuskills.com/wp-content/uploads/2017/11/html5-css3-300x200.jpg',
-        ['HTML5/CSS3', 'SASS', 'jQuery'],
-        'https://rongyadong.github.io/responsive-single-page',
-        'https://github.com/rongyadong/responsive-single-page'
-      ],
-      [
-        'https://cdn-images-1.medium.com/max/1200/1*3ZvYiNbTXCwr_-sHdhf_zA.jpeg',
-        ['HTML5/CSS3', 'Materialize', 'jQuery'],
-        'https://rongyadong.github.io/Materialize_study/index.html',
-        'https://github.com/rongyadong/Materialize_study'
-      ],
-      [
-        'https://i.ytimg.com/vi/xCGPPimXgJU/maxresdefault.jpg',
-        ['React', 'Bootstrap4', 'Axios'],
-        'https://stoic-goldwasser-51f3aa.netlify.com/',
-        'https://github.com/rongyadong/react-weather-app'
-      ],
-      [
-        'https://miro.medium.com/max/1200/0*1p4U99DAhsOHqX-m.jpg',
-        ['React', 'Redux', 'ES6'],
-        'https://cranky-yalow-46c623.netlify.com/',
-        'https://github.com/rongyadong/shopping_cart'
-      ],
-      [
-        'https://cdn-images-1.medium.com/max/2600/1*5Jv5YAGqu3zL5endb8dtBA.png',
-        ['React', 'CSS3', 'Bootstrap4'],
-        'https://quirky-lumiere-4d9e68.netlify.com',
-        'https://github.com/rongyadong/my_react_portfolio'
-      ],
-      [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX8wHLLGKUoGO02qZaDu0DJ6jDTvPrX3ZFPvUcY-r61PPqg0nlgw',
-        ['React', 'Context', 'styled-component'],
-        'https://fervent-payne-7f2b7d.netlify.com/',
-        'https://github.com/rongyadong/e-store-react-contextAPI'
-      ]
-    ]
+    projects: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      projects: data.projects
+    })
   }
 
   render() {
---
+++ src/mock/data.js
@@ -0,0 +1,40 @@
+export const data = {
+  projects: [
+    [
+      'https://www.compuskills.com/wp-content/uploads/2017/11/html5-css3-300x200.jpg',
+      ['HTML5/CSS3', 'SASS', 'jQuery'],
+      'https://rongyadong.github.io/responsive-single-page',
+      'https://github.com/rongyadong/responsive-single-page'
+    ],
+    [
+      'https://i.ytimg.com/vi/I6IY2TqnPDA/maxresdefault.jpg',
+      ['React', 'Bootstrap4', 'ES6'],
+      'https://github.com/rongyadong/advanced-react-todolist',
+      'https://elegant-shaw-5b81e0.netlify.com/'
+    ],
+    [
+      'https://i.ytimg.com/vi/xCGPPimXgJU/maxresdefault.jpg',
+      ['React', 'Bootstrap4', 'Axios'],
+      'https://stoic-goldwasser-51f3aa.netlify.com/',
+      'https://github.com/rongyadong/react-weather-app'
+    ],
+    [
+      'https://miro.medium.com/max/1200/0*1p4U99DAhsOHqX-m.jpg',
+      ['React', 'Redux', 'ES6'],
+      'https://cranky-yalow-46c623.netlify.com/',
+      'https://github.com/rongyadong/shopping_cart'
+    ],
+    [
+      'https://cdn-images-1.medium.com/max/2600/1*5Jv5YAGqu3zL5endb8dtBA.png',
+      ['React', 'CSS3', 'Bootstrap4'],
+      'https://quirky-lumiere-4d9e68.netlify.com',
+      'https://github.com/rongyadong/my_react_portfolio'
+    ],
+    [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX8wHLLGKUoGO02qZaDu0DJ6jDTvPrX3ZFPvUcY-r61PPqg0nlgw',
+      ['React', 'Context', 'styled-component'],
+      'https://fervent-payne-7f2b7d.netlify.com/',
+      'https://github.com/rongyadong/e-store-react-contextAPI'
+    ]
+  ]
+}
